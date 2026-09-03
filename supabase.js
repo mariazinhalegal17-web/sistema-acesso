8b03b0110432ae0679861e937408f7897438da8d
mariazinhalegal17-web <mariazinhalegal17@gmail.com>
2026-09-03 16:23:40 -0300

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const SUPABASE_URL = 'https://klugtgontmtbzovljcvn.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_MfCpHxlZUeW_UiCBYDAmbw_SLoLhT1P';
+const SUPABASE_ANON_KEY = 'sb_publishable_o9-s0PEdfgQ3cgpSrWXu2A_Y8mfCOFt';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
